48350956b89029de40e2f8cc42ce5b762f498e3e
saner <huhhz@hotmail.com>
2018-01-02 15:01:26 +0800

    多线程采集代理

--- app/Console/Commands/GetProxy.php
+++ app/Console/Commands/GetProxy.php
@@ -4,6 +4,7 @@ namespace App\Console\Commands;
 
 use App\Models\Proxy;
 use GuzzleHttp\Client;
+use GuzzleHttp\Pool;
 use Illuminate\Console\Command;
 use QL\QueryList;
 use Illuminate\Support\Facades\Cache;
@@ -42,9 +43,14 @@ class GetProxy extends Command
     public function handle()
     {
         Proxy::where('status', 1)->update(['status' => 0]);
-        for ($i = 1; $i <= 10; $i++) {
-            $this->getProxy($i);
+        $data = collect();
+        for ($i = 1; $i <= 20; $i++) {
+            $data = $data->merge($this->getProxy($i));
         }
+        $data = $data->unique()->filter(function ($value) {
+            return $value['time'] < 5;
+        })->values();
+        $this->save($data);
         $proxy = Proxy::where('status', 1)->orderBy('time')->take(50)->get();
         if (!empty($proxy)) {
             Cache::put('proxy_list', $proxy, 600);
@@ -53,41 +59,48 @@ class GetProxy extends Command
 
     public function getProxy(int $page)
     {
-        QueryList::get('https://proxy.coderbusy.com/zh-cn/classical/https-ready/p' . $page . '.aspx')
+       return  QueryList::get('https://proxy.coderbusy.com/zh-cn/classical/https-ready/p' . $page . '.aspx')
             ->find('tbody tr')->map(function ($value) {
                 return [
                     'ip' => $value->find('td:eq(0)')->text(),
                     'port' => $value->find('td:eq(1)')->text(),
                     'time' => str_replace('秒', '', $value->find('td:eq(9)')->text()),
                 ];
-            })->filter(function ($value) {
-                return $value['time'] < 5;
-            })->each(function ($value) {
-                if ($contents = $this->test($value)) {
-                    Proxy::create(array_merge($value, ['description' => $contents]));
-                }
             });
     }
-
-    public function test($proxy)
-    {
+    private function save($proxyList){
         $client = new Client();
         try {
-            $response = $client->get('http://myip.ipip.net/', [
-                'proxy' => [
-                    'http' => 'tcp://' . $proxy['ip'] . ':' . $proxy['port'], // Use this proxy with "http"
-                    'https' => 'tcp://' . $proxy['ip'] . ':' . $proxy['port']
-                ],
-                'connect_timeout' => 3,
-                'timeout' => 3
+            $requests = function ($proxyList) use ($client) {
+                foreach ($proxyList as $value) {
+                    yield function () use ($client, $value) {
+                        return $client->getAsync('http://myip.ipip.net/', [
+                            'proxy' => [
+                                'http' => 'tcp://' . $value['ip'] . ':' . $value['port'], // Use this proxy with "http"
+                                'https' => 'tcp://' . $value['ip'] . ':' . $value['port'],
+                            ],
+                            'connect_timeout' => 2,
+                            'timeout' => 3
+                        ]);
+                    };
+                }
+            };
+            $pool = new Pool($client, $requests($proxyList), [
+                'concurrency' => 5,
+                'fulfilled' => function ($response, $index) use ($proxyList) {
+                    $contents = $response->getBody()->getContents();
+                    if (strpos($contents, $proxyList[$index]['ip']) !== false) {
+                        Proxy::firstOrCreate(['ip'=>$proxyList[$index]['ip'],'port'=>$proxyList[$index]['port'],'status'=>1],['time'=>$proxyList[$index]['time'],'description' => $contents]);
+                    }
+                },
+                'rejected' => function ($reason, $index) {
+                    // this is delivered each failed request
+                },
             ]);
-            $contents = $response->getBody()->getContents();
-            if (strpos($contents, $proxy['ip']) === false) {
-                return false;
-            }
-            return $contents;
+            $promise = $pool->promise();
+            $promise->wait();
         } catch (\Exception $e) {
-            return false;
+
         }
     }
 }
